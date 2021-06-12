42655ab0ed5eb2b68eee6b65aa01713e67504ba1
DragonDreamer <luochong@whu.edu.cn>
2021-06-12 14:43:53 +0800

    Initial commit

---
+++ src/main/java/com/rosaecrucis/community/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.rosaecrucis.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping("/")
+	public String Index() {
+		return "index";
+	}
+}
---
+++ src/test/java/com/rosaecrucis/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosaecrucis.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
